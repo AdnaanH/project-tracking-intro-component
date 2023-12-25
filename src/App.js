df916aac6e6a30ca2f285137d3e0b9015e069b56
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-12-25 05:13:45 +0300

    Mobile menu modification and changes on mobile view

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
       <div className='w-full z-0'>
         <Header />
       </div>
-      <div className='flex min-h-screen items-start sm:items-center justify-between z-10 w-full'>
+      <div className='flex-col sm:flex-col-reverse min-h-44 sm:min-h-screen items-start sm:items-center main-sec z-10 w-full'>
         <div className="w-full sm:w-6/12">
           <SectionOne />
         </div>
         <div className="w-full sm:w-6/12">
           <SectionTwo />
         </div>
-    </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -15,33 +15,30 @@ function Header() {
       </div>
       <div className={`lg:flex gap-10 pr-28 items-start min-h-72 rounded-bl-3xl pt-14 bg-[#eef3f6] justify-end w-6/12`}>
         <ul className='hidden lg:flex gap-10 cursor-pointer list-none'>
-          <li className='text-[#5b5f71] cursor-pointer font-bold text-xl hover:text-underline'>PRODUCTS</li>
+          <li className='text-[#5b5f71] cursor-pointer font-bold text-xl hover:underline'>PRODUCTS</li>
           <li className='text-[#5b5f71] cursor-pointer font-bold text-xl'>FEATURES</li>
           <li className='text-[#5b5f71] cursor-pointer font-bold text-xl'>PRICING</li>
         </ul>
-        <div className='lg:flex gap-10'>
+        <div className='hidden sm:flex gap-10'>
           <span className='text-[#a0a1ac] font-extrabold text-2xl'>•</span>
           <h1 className='text-[#a0a1ac] cursor-pointer font-bold text-xl'>LOGIN</h1>
         </div>
       </div>
       <div className='lg:hidden flex items-start px-10 sm:px-0 py-10 sm:py-0 bg-[#eef3f6] min-h-72 gap-5 cursor-pointer' onClick={toggleMenu}>
         {isMenuOpen ? (
-          <span className='text-2xl'>&times;</span>
+          <span className='text-6xl'>&times;</span>
         ) : (
-          <span className='text-2xl'>&#9776;</span>
+          <span className='text-2xl font-extrabold'>&#9776;</span>
         )}
       </div>
       {isMenuOpen && (
-        <div className='lg:hidden flex flex-col gap-5'>
-          <ul className='flex flex-col gap-5'>
-            <li className='text-[#5b5f71] cursor-pointer font-bold text-xl hover:text-underline'>PRODUCTS</li>
-            <li className='text-[#5b5f71] cursor-pointer font-bold text-xl'>FEATURES</li>
-            <li className='text-[#5b5f71] cursor-pointer font-bold text-xl'>PRICING</li>
+        <div className='w-full flex items-start justify-center absolute top-24'>
+          <ul className='lg:hidden w-10/12  items-center p-4 flex-col bg-white'>
+            <li className='text-[#5b5f71] mb-4 cursor-pointer font-bold text-center text-xl'>PRODUCTS</li>
+            <li className='text-[#5b5f71] mb-4 cursor-pointer font-bold text-center text-xl'>FEATURES</li>
+            <li className='text-[#5b5f71] mb-4  cursor-pointer font-bold text-center text-xl'>PRICING</li>
+            <li className='text-[#a0a1ac] p-4 border-t-4 border-solid-white text-center cursor-pointer font-bold text-xl'>LOGIN</li>
           </ul>
-          <div className='flex gap-5'>
-            <span className='text-[#a0a1ac] font-extrabold text-2xl'>•</span>
-            <h1 className='text-[#a0a1ac] cursor-pointer font-bold text-xl'>LOGIN</h1>
-          </div>
         </div>
       )}
     </div>
